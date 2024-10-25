2fd6a1720d463f90f9a74fdb126da53f822dfc64
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2024-10-25 11:15:16 -0700

    Added Mad Libs

--- app.py
+++ app.py
@@ -15,8 +15,12 @@ def favorite_animal(users_animal):
     return f'Wow, {users_animal} is my favorite animal, too!'
 
 @app.route('/dessert/<users_dessert>')
-def favorite_desert(users_dessert):
+def favorite_dessert(users_dessert):
     return f'How did you know I liked {users_dessert}?'
+
+@app.route('/madlibs/<adjective>/<noun>')
+def madlibs(adjective, noun):
+    return f'One day, a {adjective} {noun} decided to enter a karaoke contest at the park. It sang so loudly, even the squirrels joined in!'
 
 if __name__ == '__main__':
     app.run(debug=True)
